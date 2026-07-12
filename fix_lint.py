fef24f7a44af624f96cb769e3dfb60262d0d9a24
PPhantomuser <PPhantomuser@users.noreply.github.com>
2026-07-12 19:58:25 +0530

    Clean up temporary python fix scripts

--- fix_lint.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('crates/lint/src/lib.rs', 'r') as f:
-    code = f.read()
-
-code = re.sub(r'\bspan\.clone\(\)', '*span', code)
-
-with open('crates/lint/src/lib.rs', 'w') as f:
-    f.write(code)
-
